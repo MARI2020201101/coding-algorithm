234af56d047370d7e73c8ab66ae87da5a739455f
MARI <mari20201101@gmail.com>
2022-12-03 13:39:45 +0900

    Practice matrix coloring with bfs

---
+++ src/ds/maxslidingwindow/MaxSlidingWindow.java
@@ -0,0 +1,13 @@
+package ds.maxslidingwindow;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class MaxSlidingWindow {
+    static int[] arr = {1,3,-1,-3,5,3,6,7};
+    static void maxSlidingWindow(int[] arr, int size){
+        Queue<Integer> queue = new PriorityQueue<>(size);
+
+    }
+}
