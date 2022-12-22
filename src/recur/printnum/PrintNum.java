ab2605578477f5628da13e19ff6f2bf78f95ca91
MARI <mari20201101@gmail.com>
2022-12-22 15:47:35 +0900

    Update print num Problem solution

--- src/recur/printnum/PrintNum.java
+++ src/recur/printnum/PrintNum.java
@@ -4,13 +4,13 @@ class PrintNum {
 
 
     public static void main(String[] args) {
-        printNum(2);
-//        printNum(3);
+//        printNum(2);
+        printNum(3);
     }
     static void printNum(int n){
         double pow = Math.pow(2, n);
         int[][] arr = new int[(int)pow][(int)pow];
-        __printNum(arr, 0, 0 ,1, (int)pow);
+        __printNum(arr, 0, 0 ,0, (int)pow);
         __print(arr);
     }
 
@@ -27,7 +27,7 @@ class PrintNum {
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 __printNum(arr, start + i*(N/2), end + j*(N/2), num, N/2);
-                num+=4;
+                num+=(N/2)*(N/2);
             }
         }
 
