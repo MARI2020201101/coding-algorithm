af6ff59bf4da0823a4affd8ecf8885070649e112
MARI <mari20201101@gmail.com>
2022-12-23 02:12:50 +0900

    Refactor find longest sequence

---
+++ src/xsolution/treeandgraph/FindLongestSequence2.java
@@ -0,0 +1,33 @@
+package xsolution.treeandgraph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class FindLongestSequence2 {
+    public static void main(String[] args) {
+        int longestSequence = findLongestSequence(1775);
+        System.out.println("longestSequence = " + longestSequence);
+    }
+    static int findLongestSequence(int n){
+       int currentLength = 0;
+       int previousLength = 0;
+       int longestSequence = 0;
+       int mask  =  1<<2;
+       while(n!=0){
+           if((n & 1)==1){
+               currentLength++;
+           }else{
+               if((n & mask)==0){
+                   previousLength = 0;
+               }else{
+                   previousLength = currentLength;
+               }
+               currentLength = 0;
+           }
+           n>>>=1;
+           longestSequence = Math.max(longestSequence, currentLength + 1 + previousLength);
+       }
+       return longestSequence;
+    }
+
+}
