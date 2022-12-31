aceb7100c73928afe9d4ba976b4e4fb48e412f9a
MARI <mari20201101@gmail.com>
2022-12-31 18:39:40 +0900

    Add new number

---
+++ src/xsolution/AddNewNumber.java
@@ -0,0 +1,27 @@
+package xsolution;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class AddNewNumber {
+    static void addNewNumber(int randomNumber){
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        if(maxHeap.size() == minHeap.size()){
+            if(randomNumber > minHeap.peek()){
+                maxHeap.add(minHeap.poll());
+                minHeap.add(randomNumber);
+            }else{
+                maxHeap.add(randomNumber);
+            }
+        }else{
+            if(randomNumber > minHeap.peek()){
+                minHeap.add(randomNumber);
+            }else{
+                minHeap.add(maxHeap.poll());
+                maxHeap.add(randomNumber);
+            }
+        }
+    }
+}
