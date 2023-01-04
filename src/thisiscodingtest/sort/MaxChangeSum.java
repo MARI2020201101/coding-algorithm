2630e65be4e16e5eb77dcd9aee641f0f511742a9
MARI <mari20201101@gmail.com>
2023-01-04 18:04:46 +0900

    Practice max exchange sum2

--- src/thisiscodingtest/sort/MaxChangeSum.java
+++ src/thisiscodingtest/sort/MaxChangeSum.java
@@ -1,8 +1,27 @@
 package thisiscodingtest.sort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class MaxChangeSum {
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.add(5);
+        a.add(4);
+        a.add(3);
+        List<Integer> b = new ArrayList<>();
+        b.add(5);
+        b.add(5);
+        b.add(6);
+        b.add(6);
+        b.add(5);
+
+        System.out.println(solve2(a, b, 3));
+    }
     static int solve(int[] a, int[] b , int k){
         int max = Arrays.stream(b).max().getAsInt();
         for (int i = 0; i < k; i++) {
@@ -13,5 +32,13 @@ class MaxChangeSum {
             a[minIdx] = max;
         }
         return Arrays.stream(a).sum();
+    }
+    static int solve2(List<Integer> a, List<Integer> b , int k){
+        Collections.sort(a);
+        Collections.sort(b, Collections.reverseOrder());
+        for (int i = 0; i < k; i++) {
+            a.set(i, b.get(i));
+        }
+        return a.stream().reduce(0, (s1, s2) -> s1+s2);
     }
 }
