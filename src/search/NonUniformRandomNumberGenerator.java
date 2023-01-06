7134369da2f290428ebc1356ed24d086f4c92f53
MARI <mari20201101@gmail.com>
2023-01-06 20:07:31 +0900

    Practice probabilities of random number generator

---
+++ src/search/NonUniformRandomNumberGenerator.java
@@ -0,0 +1,26 @@
+package search;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+class NonUniformRandomNumberGenerator {
+    static int nonUniformRandomNumberGenerator(List<Integer> values,
+                                               List<Double> probabilities){
+        List<Double> prefixSumOfProbabilities = new ArrayList<>();
+        probabilities.stream().reduce(0.0, (left, right)-> {
+            prefixSumOfProbabilities.add(left+right);
+            return left+right;
+        });
+        Random random = new Random();
+        final double uniform01 = random.nextDouble();
+        int it = Collections.binarySearch(prefixSumOfProbabilities, uniform01);
+        if(it < 0){
+            final int intervalIdx = Math.abs(it) - 1 ;
+            return values.get(intervalIdx);
+        }else{
+            return values.get(it);
+        }
+    }
+}
